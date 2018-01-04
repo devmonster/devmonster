07b62ca29bf129fbbca612b63fe0d3e68cb8da73
Ran Angelo <Ran Angelo@SURFACEPRO4>
2018-01-05 07:55:42 +0800

    Updated references for jQuery and readded font-awesome. Changed profile description

---
+++ Devmonster.me/Models/OverwatchApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Devmonster.me.Models
+{
+    public class OverwatchApiResponse
+    {
+    }
+}
